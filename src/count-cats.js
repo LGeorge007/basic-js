2343a242be8f2c397f13a7dad3435113531ce147
George <losevgeorge007@gmail.com>
2021-03-17 17:03:06 +0200

    count cats & dream team are done

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,6 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let res = JSON.stringify(matrix).match(/['"]\^{2}['"]/g);
+  return (!res && typeof(res) === "object") ? 0 : res.length;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
+/*
+module.exports = function createDreamTeam(members) {
+    let word = members.reduce((ac, el, ind) => {
+        return ac[ind] += (!el && typeof el === "object") ? "" : el[0];
+    }, []);
+    return word.sort().join("").toUpperCase();
+};
+*/
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  let word = members.map((el, ind, ar) => {
+      return ar[ind] = (typeof el === "string") ? el.trim()[0].toUpperCase() : "";
+  });
+  return word.sort().join("").toUpperCase();
 };
