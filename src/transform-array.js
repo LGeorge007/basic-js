b8b1c6cb6bb6eed8c39d0c7dca51bfb1f92e1162
George <losevgeorge007@gmail.com>
2021-03-21 14:41:29 +0200

    feat: transform-array is almost done

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error();
+  };
+  if (arr.length === 0) {
+    return [];
+  };
+  let elementsForDelete= ["--discard-next", "--discard-prev", "--double-next", "--double-prev"];
+  let resArr = arr.slice();
+  for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === "--discard-next" && (i < arr.length - 1)) {
+        resArr.splice(i+1, 1);
+      } else if (arr[i] === "--double-next" && (i < arr.length - 1)) {
+        resArr.splice((i+2), 0, resArr[i+1]);
+
+      } else if (arr[i] === "--discard-prev" && i !== 0) {
+        resArr.splice((i-1), 1);
+      } else if (arr[i] === "--double-prev" && i !== 0) {
+        resArr.splice((i-2), 0, resArr[i-1]);
+
+      };
+     }
+  return resArr.filter(el => {
+      return !elementsForDelete.includes(el);
+  });
 };
