10a51d30c30d4a84b293fd1cc900270fa16c8317
George <losevgeorge007@gmail.com>
2021-03-31 01:32:29 +0300

    feat viginere cipher was done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,26 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
-}
-
-/*
-class VigenereCipheringMachine {
-
-
   constructor(direction = true) {
     this.direction = direction;
   }
 
   encrypt(message, key) {
-    //Проверка на undefined
+     //Проверка на undefined
     if (message === undefined || key === undefined) {
       throw new Error();
     };
@@ -28,6 +14,7 @@ class VigenereCipheringMachine {
     let dictionary = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
     //Numerical Массив от message
     let encryptedArrayFromMessage = [];
+
     message.toUpperCase().split("").forEach(e => {
       let val = dictionary.indexOf(e);
       encryptedArrayFromMessage.push((val === -1) ? e : val);
@@ -38,7 +25,7 @@ class VigenereCipheringMachine {
       let val = dictionary.indexOf(e);
       encryptedKey.push((val === -1) ? e : val);
     });
-    numChipherArrayFromMessage = [];
+    let numChipherArrayFromMessage = [];
     //Идем по сообщению
     let j = 0;
     for (let i = 0; i < encryptedArrayFromMessage.length; i++) {
@@ -61,65 +48,75 @@ class VigenereCipheringMachine {
       let val = dictionary[e];
       letterChipherArrayFromMessage.push((val !== undefined) ? val : e);
     });
-    if (this.direction = true) {
+
+    if (this.direction) {
       return letterChipherArrayFromMessage.join("");
     } else {
       return letterChipherArrayFromMessage.reverse().join("");
     };
+
   }
 
-  decrypt(encryptedMessage, key) {
-    if (encryptedMessage === undefined || key === undefined) {
-      throw new Error();
-    };
-    //Словарь
-    let dictionary = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-    //ключ
-    let encryptedKey = [];
-    key.toUpperCase().split("").forEach(e => {
-      let val = dictionary.indexOf(e);
-      encryptedKey.push((val === -1) ? e : val);
-    });
-    //Numerical Массив от message
-    let encryptedArrayFromMessage = [];
-    if (this.direction = true) {
-      encryptedMessage.toUpperCase().split("").reverse().forEach(e => {
-        let val = dictionary.indexOf(e);
-        encryptedArrayFromMessage.push((val === -1) ? e : val);
-      });
-    } else {
-      encryptedMessage.toUpperCase().split("").forEach(e => {
-        let val = dictionary.indexOf(e);
-        encryptedArrayFromMessage.push((val === -1) ? e : val);
-      });
-    };
-    //
-    numChipherArrayFromMessage = [];
-    //Идем по сообщению
-    let j = 0;
-    for (let i = 0; i < encryptedArrayFromMessage.length; i++) {
-      let letterM = encryptedArrayFromMessage[i];
-      if (letterM === " " || letterM === "!" || letterM === "." || letterM === ",") {
-        numChipherArrayFromMessage.push(letterM);
-        continue;
+  decrypt(message, key) {
+
+     if (message === undefined || key === undefined) {
+        throw new Error();
       };
-      let letterK = encryptedKey[j];
-      if (j !== encryptedKey.length - 1) {
-        j++;
+      //Словарь
+      let dictionary = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+      //ключ
+      let encryptedKey = [];
+      key.toUpperCase().split("").forEach(e => {
+        let val = dictionary.indexOf(e);
+        encryptedKey.push((val === -1) ? e : val);
+      });
+      //Numerical Массив от message
+      let encryptedArrayFromMessage = [];
+      if (this.direction) {
+        message.toUpperCase().split("").forEach(e => {
+          let val = dictionary.indexOf(e);
+          encryptedArrayFromMessage.push((val === -1) ? e : val);
+        });
+        //console.log("Прямой ", encryptedArrayFromMessage);
       } else {
-        j = 0;
+        message.toUpperCase().split("").reverse().forEach(e => {
+          let val = dictionary.indexOf(e);
+          encryptedArrayFromMessage.push((val === -1) ? e : val);
+        });
+        //console.log("НЕПрямой ", encryptedArrayFromMessage);
       };
-      //добавляем цифру шифра в массив
-      numChipherArrayFromMessage.push((26 - letterK + letterM) % 26);
-    }
-    let letterChipherArrayFromMessage = [];
-    numChipherArrayFromMessage.forEach((e) => {
-      let val = dictionary[e];
-      letterChipherArrayFromMessage.push((val !== undefined) ? val : e);
-    });
-    return letterChipherArrayFromMessage.join("");
-  }
-  */
+      //
+      let numChipherArrayFromMessage = [];
+      //Идем по сообщению
+      let j = 0;
+      for (let i = 0; i < encryptedArrayFromMessage.length; i++) {
+        let letterM = encryptedArrayFromMessage[i];
+        if (letterM === " " || letterM === "!" || letterM === "." || letterM === "," || letterM === ":" || letterM === ")" || letterM === "(" || letterM === "/") {
+          numChipherArrayFromMessage.push(letterM);
+          continue;
+        };
+        let letterK = encryptedKey[j];
+        if (j !== encryptedKey.length - 1) {
+          j++;
+        } else {
+          j = 0;
+        };
+        //добавляем цифру шифра в массив
+        numChipherArrayFromMessage.push((26 - letterK + letterM) % 26);
+      }
+      let letterChipherArrayFromMessage = [];
+      numChipherArrayFromMessage.forEach((e) => {
+        let val = dictionary[e];
+        letterChipherArrayFromMessage.push((val !== undefined) ? val : e);
+      });
 
+      if (this.direction) {
+        return letterChipherArrayFromMessage.join("");
+      } else {
+        return letterChipherArrayFromMessage.reverse().join('');
+      };
+
+  }
+}
 
 module.exports = VigenereCipheringMachine;
